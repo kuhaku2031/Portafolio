85bd485bcbe21152c19d7042d36fb2639ddf0e18
kuhaku2031 <juanmanuelcontreraszapata33@gmail.com>
2025-05-06 23:05:14 -0500

    first commit

---
+++ src/components/tecnologies.js
@@ -0,0 +1,14 @@
+export const experiences = [
+  {
+    company: 'Metacortex',
+    role: 'Programador',
+    description: 'Programa software respetable desde un cubículo.',
+    year: '1999 - Actualidad',
+  },
+  {
+    company: 'Nebuchadnezzar',
+    role: 'El Elegido',
+    description: 'Me encargo de salvar el mundo.',
+    year: '1999 - Actualidad',
+  },
+];
