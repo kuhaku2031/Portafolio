6f129c2d313bcdd38788ead006b1b54186b120d8
kuhaku2031 <juanmanuelcontreraszapata33@gmail.com>
2025-05-11 15:19:35 -0500

    Arrelgo de proyectos

--- src/scripts/proyects.js
+++ src/scripts/proyects.js
@@ -1,50 +1,28 @@
-export const proyects = [
+const proyects = [
   {
     title: 'Software random',
     owner: 'Programador',
     description: 'Programa software respetable desde un cubículo.',
-    tecnologies: [
-      "node","next",
-    ],
-    links: [
-      {github: "/root"},
-      {web: "/web"}
-    ]
+    tecnologies: ["node","next"],
+    links: ["/root","/github"],
+    img:"/me/makis-hristaras-SkG3lysMrFo-unsplash.jpg" 
   },
-    {
+  {
     title: 'Software random',
     owner: 'Programador',
     description: 'Programa software respetable desde un cubículo.',
-    tecnologies: [
-      "node","next",
-    ],
-    links: [
-      {github: "/root"},
-      {web: "/web"}
-    ]
+    tecnologies: ["node","next"],
+    links: ["/root","/github"],
+    img:"/me/makis-hristaras-SkG3lysMrFo-unsplash.jpg" 
   },
-    {
+  {
     title: 'Software random',
     owner: 'Programador',
     description: 'Programa software respetable desde un cubículo.',
-    tecnologies: [
-      "node","next",
-    ],
-    links: [
-      {github: "/root"},
-      {web: "/web"}
-    ]
-  },
-    {
-    title: 'Software random',
-    owner: 'Programador',
-    description: 'Programa software respetable desde un cubículo.',
-    tecnologies: [
-      "node","next",
-    ],
-    links: [
-      {github: "/root"},
-      {web: "/web"}
-    ]
+    tecnologies: ["node","next"],
+    links: ["/root","/github"],
+    img:"/me/makis-hristaras-SkG3lysMrFo-unsplash.jpg" 
   },
 ];
+
+export default proyects;
