0d1b87e7516983e92ab5d6526e6bd2a06779bf43
kuhaku2031 <juanmanuelcontreraszapata33@gmail.com>
2026-01-09 19:17:59 -0500

    remove(Porfolio): delete duplicated projects

--- Porfolio/src/app/about/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-
-export const Route = createFileRoute("/about/")({
-  component: AboutPage,
-});
-
-function AboutPage() {
-  return (<div className="p-4">About NexoAI</div>);
-}
